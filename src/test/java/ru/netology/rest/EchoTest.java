bfa55af73215d7a485d8ab4da5a3a6ea2018c151
ValeryCharkin <charkin.valery@gmail.com>
2024-11-04 19:49:39 +0300

    test failed

--- src/test/java/ru/netology/rest/EchoTest.java
+++ src/test/java/ru/netology/rest/EchoTest.java
@@ -12,7 +12,7 @@ public class EchoTest {
         given()
                 .baseUri("https://postman-echo.com")
                 .contentType("text/plain; charset=UTF-8")
-                .body("Hello")
+                .body("Ciao")
                 .when().log().all()
                 .post("/post")
                 .then()
